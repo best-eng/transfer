69655fabf0b28ad41e28e08f68a8f04292ab4ab3
best-eng <segarus2004@gmail.com>
2026-05-24 14:06:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN      = "8759629143:AAGJYB5nK1fy-gPxuVXxkgyA88vAE6QL_qY"
+BOT_TOKEN      = "8878893944:AAHt6WUOLMcxpHNmfsyhyCYo6VT6fOAdl5g"
 ADMIN_CHAT_IDS = [1445696823]
 WEBAPP_URL = "https://best-eng.github.io/transfer"
